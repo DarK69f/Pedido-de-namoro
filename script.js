af454ac92afe1fce9d95ab3aa38095b3459efe17
DarK69f <Pedrocastrotuti@outlook.com>
2025-10-02 00:03:24 -0300

    2.0

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function Ebaa() {
-    document.getElementById("elaAceitou").innerHTML = "EBAAA 🥰";
+    document.getElementById("elaAceitou").innerHTML = "EBAAA 🥰 <br> Aqui está meu número:<br> +55 99 99999 9999";
     elementToRemove = document.getElementById("Nem_pense")
     elementToRemove.remove();
+
+    elementToRemove1 = document.getElementById("YesBaby")
+    elementToRemove1.remove();
 }
 
 function Nananina_nao() {
@@ -21,4 +24,19 @@ function Nananina_nao() {
 
 function Droga() {
     document.getElementById("elaAceitou").innerHTML = "Tudo bem, se é assim então... 😭😥"
+    elementToRemove = document.getElementById("YesBaby")
+    elementToRemove.remove();
+
+    elementToRemove1 = document.getElementById("Nem_pense")
+    elementToRemove1.remove();
+}
+
+let hora = new Date().getHours();
+
+if (hora < 12) {
+    document.getElementById("dia").innerHTML = "Eiii, bom dia 🌞. <br> Eu tenho uma pergunta!"
+} else if (hora < 18) {
+    document.getElementById("dia").innerHTML = "Eiii, boa tarde ⛅. <br> Eu tenho uma pergunta!"
+} else {
+    document.getElementById("dia").innerHTML = "Eiii, boa noite 🌙. <br> Eu tenho uma pergunta!"
 }
